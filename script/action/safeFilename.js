1c618833ad91c1fd390a8ff6b4fc276589567b44
Pat Kujawa <pat.kujawa@webfilings.com>
2016-08-16 10:36:04 -0600

    Update safe filename script to use unicode for some url chars.

--- script/action/safeFilename.js
+++ script/action/safeFilename.js
@@ -1,5 +1,6 @@
 // Make safe for dropbox, at least
 // [Why aren't certain files on one computer syncing to another? (Dropbox Help Center)](https://www.dropbox.com/en/help/145)
+// Also, keep question marks and ampersands, but do so by making them unicode
 testInputs = {
     'allBad': '/\\<>:"|?*',
     'spaceAtEnd': ' ',
@@ -8,12 +9,16 @@ testInputs = {
     'temp2': '.~a',
     'temp3': '~a.tmp'
 }
+questionMarks = /[?]/g
+ampersands = /[&]/g
 dateOrPathSeparators = /[\/\\]/g
 allBadRE = /[\/\\<>:"|?*]/g
 endingsRE = /[\s.]+$/
 tempsRE = /^~\$|\.~/
 tempRE = /^~(.*)\.tmp/
 return clipText
+    .replace(questionMarks, '﹖')
+    .replace(ampersands, '﹠')
     .replace(dateOrPathSeparators, '-')
     .replace(allBadRE, '')
     .replace(endingsRE, '')
